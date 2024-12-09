0aec433f5b2d081eec489eb58d06c90f3766fd5b
Wilmer <wpgaray21@gmail.com>
2024-12-09 12:20:09 -0500

    Agregar configuración completa de Flask-Migrate

--- factory.py
+++ factory.py
@@ -1,37 +1,37 @@
 # factory.py
 import os
 from flask import Flask
-from extensions import db, migrate, socketio, csrf
+from extensions import db, socketio, csrf
+from flask_migrate import Migrate
 from clients.routes import client_bp
 from administracion.routes import admin_bp
-from main_routes import register_routes
-from flask_wtf.csrf import generate_csrf
 from main_routes import main_bp
+from flask_wtf.csrf import generate_csrf
 from dotenv import load_dotenv
+
+
 def create_app():
     app = Flask(__name__)
+    load_dotenv()  # Cargar variables de entorno desde el archivo .env
 
     # Configuración de la aplicación
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
         'DATABASE_URL',
-        'sqlite:///apartamentos.db'
+        'sqlite:///apartamentos.db'  # Base de datos SQLite por defecto si no se encuentra DATABASE_URL
     )
-
-
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.secret_key = 'una_clave_secreta_muy_segura_y_unica'
-    app.config['WTF_CSRF_HEADERS'] = ['X-CSRFToken'] 
+    app.secret_key = os.environ.get('62aa283ac8862da4fa6a2bc7fdba7d83b5a577818c568abb')
+    app.config['WTF_CSRF_HEADERS'] = ['X-CSRFToken']
     app.config['UPLOAD_FOLDER'] = os.path.join('static', 'uploads')
     os.makedirs(app.config['UPLOAD_FOLDER'], exist_ok=True)
 
     # Inicialización de extensiones
     db.init_app(app)
-    migrate.init_app(app, db)
+    migrate = Migrate(app, db)  # Inicializar Flask-Migrate
     socketio.init_app(app, cors_allowed_origins=os.environ.get('CORS_ALLOWED_ORIGINS', '*'), manage_session=False)
     csrf.init_app(app)
-    load_dotenv()
-    
-    # Registrar rutas
+
+    # Registrar Blueprints
     app.register_blueprint(main_bp)
     app.register_blueprint(client_bp, url_prefix='/clients')
     app.register_blueprint(admin_bp, url_prefix='/administracion')
@@ -43,4 +43,3 @@ def create_app():
     app.jinja_env.globals['csrf_token'] = generate_csrf_token
 
     return app
-
